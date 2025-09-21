8317048af5d085980ca1e09e1a68be6ad795c00d
Natalie Danner <danner15@usf.edu>
2025-09-21 00:40:45 -0400

    Simulated checkout process

--- cc_4.js
+++ cc_4.js
@@ -1,4 +1,4 @@
-let product = [
+let products = [
     {
         name: "Bread",
         category: "Groceries",
@@ -29,13 +29,13 @@ let product = [
        price: 150,
        inventory: 10,
     },
-]
+];
 // Add dynamic discount
 
 for (let product of products) {
     let discount = 0
 
-    switch(category) {
+    switch(product.category) {
         case "Groceries":
         case "Household":
             product.price *= 0.9; // 10% off groceries and household
@@ -50,25 +50,69 @@ for (let product of products) {
             discount = 0;
     } 
 }
-
+console.log(products[0].price);
 // Create customerType discount
 
 let customerType = "student";
 let extraDiscount = 0;
 if (customerType === "student") {
-    extraDiscount = 0.05;
+    extraDiscount = 0.05;  // 5% discount for students
 }
 else if (customerType === "senior") {
-    extraDiscount = 0.07;
+    extraDiscount = 0.07;  // 7% discount for seniors
 }
 else {extraDiscount = 0};
 
 // Simulate checkout process
 
+let customers = [
+    {
+        number: "001",
+        customerType: "student",
+        cart: "Bread",
+    },
+     {
+        number: "002",
+        customerType: "senior",
+        cart: "Lamp",
+    },
+    {
+        number: "003",
+        customerType: "regular",
+        cart: "Hoodie",
+    },
+]
+for (let i = 1; i <= customers.length; i++) {
 let subtotal = 0;
-for (let i = 1; i <= 3; i++) {
-    subtotal += product.price;
-    (product.invetory--)
+
+for (let customer of customers) {
+    
+    switch (customer.cart) {
+    case "Bread": 
+        subtotal += (products[0].price);
+        (products[0].inventory--);
+        break;
+    case "Hoodie": 
+        subtotal += (products[1].price);
+        (products[1].inventory--);
+        break;
+    case "Tomato": 
+        subtotal += (products[2].price);
+        (products[2].inventory--);
+        break;
+    case "Lamp":
+        subtotal += (products[3].price);
+        (products[3].inventory--);
+        break;
+    case "TV":
+        subtotal += (products[4].price);
+        (products[4].inventory--);
+        break;
+    default:
+        subtotal = 0
+}}
+
 let finalTotal = subtotal * (1 - extraDiscount);
 console.log(`Customer ${i}: $${finalTotal.toFixed(2)}`);
+console.log()
 }
